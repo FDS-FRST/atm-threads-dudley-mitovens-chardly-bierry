b1163c80f0eb7c86d9ccef07b9ea2fa7e79c1409
steph bierry <fontilmestep@gmail.com>
2025-12-20 23:01:32 -0500

    Premiere partie partie du devoir d'ATM achevee , Bierry sur branche-Bierry

---
+++ src/main/java/ht/ueh/first/java/BankAccount.java
@@ -0,0 +1,43 @@
+package ht.ueh.first.java;
+
+public class BankAccount {
+ private int accountNumber;
+ private double balance;
+
+public BankAccount(int accountNumber, double balance){
+    this.accountNumber = accountNumber;
+    this.balance       = balance;
+  
+}
+public int  getaccountNumber() {
+    return accountNumber;
+}
+public  void setaccountnumber(int accountNumber){
+    this.accountNumber = accountNumber;
+}
+
+ public double getbalance() {
+    return balance;
+ }
+ public void setbalance(double balance){
+    this.balance = balance;
+ }
+
+public double deposit (double amount) {
+    balance = amount + getbalance();
+   System.out.println("La nouvelle balance est : " + balance );
+   return balance;
+}
+public double withdraw (double amount){
+if (amount < balance)
+{
+     balance = getbalance() - amount;
+
+    
+}
+ System.out.println(" Vous n'avez pas assez d'argent pour faire ce retrait.");
+    System.out.println("Votre somme est : " + balance + " et vous ne pouvez reitirer la somme de  " + amount );
+    return balance;
+}
+
+}
--- src/main/java/ht/ueh/first/java/Main.java
+++ src/main/java/ht/ueh/first/java/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("Please see the readme for your assignation");
+
+    BankAccount Pierre = new BankAccount (123, 1000);
+    Pierre.deposit(125);
+    Pierre.withdraw(2000);
+   
     }
+
 }
